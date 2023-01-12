438c905ab9a7e6ddcafdca154b48f0cc142f8b65
nithinacharya11 <nithinpacharya@gmail.com>
2023-01-12 10:40:16 +0530

    initial commit

---
+++ Javascript Assignments/1/variable.js
@@ -0,0 +1,15 @@
+
+let name = "Nithin";
+let age = 22;
+let isStudent = true;
+let hobbies = ["Singing", "Drawing", "Photography"];
+let location = null;
+let contact = undefined;
+
+
+console.log("name:", name, typeof name);
+console.log("age:", age, typeof age); 
+console.log("isStudent:", isStudent, typeof isStudent); 
+console.log("courses:", courses, typeof hobbies); 
+console.log("location:", location, typeof location); 
+console.log("contact:", contact, typeof contact); 
