e8c961dbb772bb38b898303a7d3d602ae8a63905
nithinacharya11 <nithinpacharya@gmail.com>
2023-01-13 11:22:47 +0530

    initial commit

--- Javascript Assignments/1/variable.js
+++ Javascript Assignments/1/variable.js
@@ -1,3 +1,5 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
 
 let name = "Nithin";
 let age = 22;
@@ -7,9 +9,9 @@ let location = null;
 let contact = undefined;
 
 
-console.log("name:", name, typeof name);
-console.log("age:", age, typeof age); 
-console.log("isStudent:", isStudent, typeof isStudent); 
-console.log("courses:", courses, typeof hobbies); 
-console.log("location:", location, typeof location); 
-console.log("contact:", contact, typeof contact); 
+console.log("name:", name, "Data type:", typeof name);
+console.log("age:", age, "Data type:", typeof age); 
+console.log("isStudent:", isStudent, "Data type:", typeof isStudent); 
+console.log("hobbies:", hobbies, "Data type:", typeof hobbies); 
+console.log("location:", location, "Data type:", typeof location); 
+console.log("contact:", contact, "Data type:", typeof contact); 
