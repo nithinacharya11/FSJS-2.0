5a4338756d1bb3c1ae1d661e816273be94c4577b
nithinacharya11 <nithinpacharya@gmail.com>
2023-01-18 20:38:30 +0530

    Final commit

--- Javascript Assignments/18 question/variable.js
+++ Javascript Assignments/18 question/variable.js
@@ -1,3 +1,8 @@
 // 18. Write a program which tells the number of days in a month, now consider leap year.
 
+function daysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
+
+console.log(daysInMonth(2022, 2));  // 28 or 29
 
